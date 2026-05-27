9b3c6a3eab19f24f4ff6723e5770f0a25f60838b
MateoBaute <mateobaute10@gmail.com>
2026-05-27 18:36:15 -0300

    avance login

---
+++ js/LoginScripts.js
@@ -0,0 +1,7 @@
+
+function login(){
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+    let email = document.getElementById("email").value;
+    
+}
