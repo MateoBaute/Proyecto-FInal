15956ff78587cc076fab776d8bf0402248170668
MateoBaute <mateobaute10@gmail.com>
2026-05-28 16:44:25 -0300

    Login

--- js/LoginScripts.js
+++ js/LoginScripts.js
@@ -3,5 +3,30 @@ function login(){
     let username = document.getElementById("username").value;
     let password = document.getElementById("password").value;
     let email = document.getElementById("email").value;
-    
+    let url = "http://localhost:8080/login?username=" + username + "&password=" + password + "&email=" + email;
+
+    let user = {
+        name: username,
+        pass: password,
+        mail: email
+    }
+    console.log(user)
+if(!user){
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        data.forEach(user => {
+            if(user.username === username && user.password === password && user.email === email){
+                alert("Inicio de sesion correcto");
+                window.location.href = "index.html";
+            } else {
+                alert("Datos invalidos, intenta nuevamente.");
+            }
+    })
+})
+    .catch(error => {
+        console.error("Error:", error);
+        alert("Un error ocurrio durante el inicio de sesion. Intenta nuevamente.");
+    });
+  }
 }
