dc82a152b72682d7da1fd1052f85a91bc3de4bb2
MateoBaute <mateobaute10@gmail.com>
2026-06-03 16:46:29 -0300

    prueba baute

--- js/LoginScripts.js
+++ js/LoginScripts.js
@@ -1,5 +1,5 @@
 
-function login(){
+function login() {
     let username = document.getElementById("username").value;
     let password = document.getElementById("password").value;
     let email = document.getElementById("email").value;
@@ -11,22 +11,22 @@ function login(){
         mail: email
     }
     console.log(user)
-if(!user){
-    fetch(url)
-    .then(response => response.json())
-    .then(data => {
-        data.forEach(user => {
-            if(user.username === username && user.password === password && user.email === email){
-                alert("Inicio de sesion correcto");
-                window.location.href = "index.html";
-            } else {
-                alert("Datos invalidos, intenta nuevamente.");
-            }
-    })
-})
-    .catch(error => {
-        console.error("Error:", error);
-        alert("Un error ocurrio durante el inicio de sesion. Intenta nuevamente.");
-    });
-  }
+    if (!user) {
+        fetch(url)
+            .then(response => response.json())
+            .then(data => {
+                data.forEach(user => {
+                    if (user.username === username && user.password === password && user.email === email) {
+                        alert("Inicio de sesion correcto");
+                        window.location.href = "index.html";
+                    } else {
+                        alert("Datos invalidos, intenta nuevamente.");
+                    }
+                })
+            })
+            .catch(error => {
+                console.error("Error:", error);
+                alert("Un error ocurrio durante el inicio de sesion. Intenta nuevamente.");
+            });
+    }
 }
