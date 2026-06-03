627c313d183e2a60cc225735db96b0c69a1bafa2
DyldeadorV2 <dylanfcitoflow@gmail.com>
2026-06-03 18:21:16 -0300

    Avances Rutina y login

---
+++ js/RutinasScripts.js
@@ -0,0 +1,10 @@
+function MostrarFrm (){
+ const FrmCont = document.getElementById("FrmCrearRutina")
+ FrmCont.innerHTML = `<div>
+ <h1>  </h1>
+    <input id="" type="" >
+        <input id="" type="" >
+            <input id="" type="" >
+                <input id="" type="" >
+ </div>`
+}
